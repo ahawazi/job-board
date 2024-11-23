a9f51c7cc992812c15d4f5aafd3f125549b69499
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-11-23 16:12:10 +0330

    feat (page) : add about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<x-site-layout>
+    <div class="flex justify-center gap-4">
+        <x-nav-link :href="route('home')">Home</x-nav-link>
+        <x-nav-link :href="route('about')">About</x-nav-link>
+        <x-nav-link :href="route('contact')">Contact</x-nav-link>
+    </div>
+    <h1 class="text-center space-y-3">at contact</h1>
+</x-site-layout>
