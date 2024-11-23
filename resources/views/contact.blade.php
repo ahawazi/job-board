731345edc0ad4d9caa66d1efb82582cae947e8d6
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-11-23 16:33:55 +0330

    fix (page) : fix thr dark mode

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,11 +1,6 @@
 <x-site-layout>
-    <div class="flex justify-center gap-4">
-        <x-nav-link :href="route('home')">Home</x-nav-link>
-        <x-nav-link :href="route('about')">About</x-nav-link>
-        <x-nav-link :href="route('contact')">Contact</x-nav-link>
-    </div>
-
-    
-
-    <h1 class="text-center space-y-3">at contact</h1>
+    <x-slot:heading>
+        contact page
+    </x-slot:heading>
+    <h1 class="font-bold">at contact</h1>
 </x-site-layout>
