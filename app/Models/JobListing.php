1f117568cc20adb82c2f51d37a67b9748cc2568d
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-11-25 13:45:24 +0330

    feat (joblisting) : add relation between employer

--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class JobListing extends Model
 {
@@ -14,5 +15,10 @@ class JobListing extends Model
         'title',
         'salary',
     ];
+
+    public function employer():BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 
 }
