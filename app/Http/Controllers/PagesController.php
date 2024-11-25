d69b1d5f196d42411e1d605f834a0c718bdd229f
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-11-25 12:16:16 +0330

    fix (controller) : page controller move job methods

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,31 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Job;
-use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
-
 class PagesController extends Controller
 {
     public function home()
     {
         return view('home');
-    }
-
-    public function jobs()
-    {
-        return view('jobs', [
-            'jobs' => Job::all(),
-        ]);
-    }
-
-    public function job($id)
-    {
-        $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
-        return view('job', [
-            'job' => $job, 
-        ]);
-
     }
 
     public function contact()
