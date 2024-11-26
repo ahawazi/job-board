b161cc6f4ad5b2c36b47aa20d29cefbe3fce2d0c
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-11-26 16:05:46 +0330

    ref (jobs) : ref the UI

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,15 +1,17 @@
 <x-site-layout>
     <x-slot:heading>
-        Jobs page
+        Job Listing
     </x-slot:heading>
-    <ul>
+
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li class="text-white">
-                {{-- @dd($job) --}}
-                <a href="{{ route('job', $job['id']) }}" class="text-white">
-                    <p>{{ $job['title'] }}: Pays {{ $job['salary'] }} per years.</p>
-                </a>
-            </li>
+            <a href="{{ route('job', $job['id']) }}"
+                class="block px-4 py-6 border-gray-200 rounded-lg dark:border-white dark:bg-gray-600">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+                <div>
+                    <strong class="dark:text-gray-700 dark:font-bold">{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-site-layout>
