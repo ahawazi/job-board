f90e6a3098e10e3525cb9e1dc9bf0058a1d2a5c7
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-11-27 18:44:24 +0330

    fix (page) : add pages for jobs

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -4,8 +4,9 @@
     </x-slot:heading>
 
     <div class="space-y-4">
+        <x-link-button-green href="{{ route('jobs.create') }}">Create Job</x-link-button-green>
         @foreach ($jobs as $job)
-            <a href="{{ route('job', $job['id']) }}"
+            <a href="{{ route('jobs.show', $job['id']) }}"
                 class="block px-4 py-6 border-gray-200 rounded-lg dark:border-white dark:bg-gray-600">
                 <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
                 <div>
