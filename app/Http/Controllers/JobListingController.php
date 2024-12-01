49fe9a12685867b9a7e981514f75a13b3fca3ba9
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-12-01 18:34:44 +0330

    feat : add story to jobslistingcontroller

--- app/Http/Controllers/JobListingController.php
+++ app/Http/Controllers/JobListingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreJobRequest;
 use App\Models\JobListing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
@@ -13,13 +14,23 @@ class JobListingController extends Controller
         return view('jobs.index', [
             // 'jobs' => JobListing::with('employer')->paginate(3),
             // 'jobs' => JobListing::with('employer')->simplePaginate(3),
-            'jobs' => JobListing::with('employer')->cursorPaginate(3),
+            'jobs' => JobListing::with('employer')->latest()->simplePaginate(3),
         ]);
     }
 
     public function create(Request $request)
     {
         return view('jobs.create');
+    }
+
+    public function store(StoreJobRequest $request)
+    {
+        $request->user()->jobsListing()->create([
+            'title' => $request->input('title'),
+            'salary' => $request->input('salary'),
+            'employer_id' => 1,
+        ]);
+        return to_route('jobs.index');
     }
 
     public function show(JobListing $job)
