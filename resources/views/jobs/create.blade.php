fb51c61faffdea4ad53c4e82b0c4dd0bbed7d568
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-12-02 14:59:45 +0330

    add error create

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -18,7 +18,7 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="title" id="title"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
-                                    placeholder="Shift Leader">
+                                    placeholder="Shift Leader" required>
                             </div>
                         </div>
                     </div>
@@ -30,11 +30,22 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset dark:ring-white focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="salary" id="salary"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6"
-                                    placeholder="$50,000 Per Year">
+                                    placeholder="$50,000 Per Year" required>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <div class="mt-10">
+                    @if ($errors->any())
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li class="text-red-500 italic">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </div>
+
             </div>
         </div>
 
