35dde79c750987e23780c1069218c9b51332b3ce
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-12-05 13:49:46 +0330

    feat (test) : jobpagetest

---
+++ tests/Feature/pages/JobPageTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\Job;
+
+use function Pest\Laravel\get;
+
+it ('show Jobs', function() {
+    $job = Job::factory()->create();
+
+    get('/jobs')
+    ->assertSee($job->title)
+    ->assertSee($job->salary);
+});
+
+it ('show single Job', function() {
+    $job = Job::factory()->create();
+
+    get(route('jobs.show', $job))
+    ->assertSee($job->title)
+    ->assertSee($job->salary);
+});
