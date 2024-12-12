e770018ea2e016b6634f260ce118861e010edd8e
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-12-12 19:19:18 +0330

    feat : complite the employer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            EmployerSeeder::class,
             UserSeeder::class,
+            EmployerSeeder::class,
             JobSeeder::class,
         ]);
     }
