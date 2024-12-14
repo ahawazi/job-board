3df82cd77dd62fc959951461f4a7ae1d23563d6b
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-12-14 14:27:40 +0330

    fix style

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -6,8 +6,8 @@
     <div class="space-y-4">
         @foreach ($jobs as $job)
             <a href="{{ route('jobs.show', $job['id']) }}"
-                class="block px-4 py-6 border-gray-200 rounded-lg dark:border-white dark:bg-gray-600">
-                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+                class="block px-4 py-6 border border-gray-200 rounded-lg dark:border-white dark:bg-gray-600">
+                <div class="font-bold text-blue-500 text-sm dark:text-gray-700">{{ $job->employer->name }}</div>
                 <div>
                     <strong class="dark:text-gray-700 dark:font-bold">{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
                 </div>
