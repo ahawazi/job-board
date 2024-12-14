f8373308e7da12939d9a593cd6f6160da4505579
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-12-14 13:28:45 +0330

    fix : create page for job

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -14,7 +14,7 @@
                     <x-form-field>
                         <x-form-lable for="title">Title</x-form-lable>
                         <div class="mt-2">
-                            <x-form-input name="title" id="title" placeholder="Title" required />
+                            <x-form-input name="title" id="title" placeholder="title" required />
                             <x-form-error name="title" />
                         </div>
                     </x-form-field>
@@ -22,7 +22,7 @@
                     <x-form-field>
                         <x-form-lable for="salary">Salary</x-form-lable>
                         <div class="mt-2">
-                            <x-form-input name="salary" id="salary" placeholder="CEO" required />
+                            <x-form-input name="salary" id="salary" placeholder="salary" required />
                             <x-form-error name="salary" />
                         </div>
                     </x-form-field>
